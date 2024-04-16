2d0b0d0d99ae1b3574243cc8bce9956239096cf1
JU4N-DEV <juaningo03@gmail.com>
2024-04-16 00:13:55 -0600

    UPDATE 5

--- src/modules/db.js
+++ src/modules/db.js
@@ -4,19 +4,23 @@ import { getAuth } from "firebase/auth";
 import {getDatabase} from "firebase/database";
 
 import {getFirestore} from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGEB,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER,
-  appId: import.meta.env.APP_ID,
-  measurementId:import.meta.env.MESURE_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGEB,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId:import.meta.env.VITE_MESURE_ID
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
