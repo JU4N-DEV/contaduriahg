6b0714938bbac4ee545f19951280fbe1f8c57295
JU4N-DEV <juaningo03@gmail.com>
2024-04-22 14:02:38 -0600

    Update map ps

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import './index.css'
 
-import 'mapbox-gl/dist/mapbox-gl.css';
+import './mapboxcss/mapbox-gl.css';
 
 /**
  * const createRouter = createBrowserRouter([
