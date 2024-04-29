9b6bff7b84a88909e0579b0f3c1dce52dce5220b
JU4N-DEV <juaningo03@gmail.com>
2024-04-29 12:02:34 -0600

    Correcion decimo errors

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -78,7 +78,7 @@ export default function MapView(){
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-  
+    
     </MapContainer>
     </>
     
